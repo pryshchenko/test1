883203611bad84e44a403194c0dc55f64b57ec30
pryshchenko <pryshchenko.d@gmail.com>
2024-02-09 02:23:31 +0300

    test

--- script.js
+++ script.js
@@ -5,31 +5,64 @@
   const playerContainer = document.getElementById('player')
   const modalContent = document.querySelector('.modal-content')
   const pagi = document.querySelector('.pagination')
+  const leftArrow = document.querySelector('.leftArrow')
+  const rightArrow = document.querySelector('.rightArrow')
+  
+  let active
 
   const videos = [
-    { url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
-    { url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
-    { url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
-    { url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
-    { url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
-    { url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
-    { url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
-    { url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' }
+    { id: 0, url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
+    { id: 1, url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
+    { id: 2, url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
+    { id: 3, url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
+    { id: 4, url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
+    { id: 5, url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
+    { id: 6, url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' },
+    { id: 7, url: '824804225', thumbnail: 'https://i.vimeocdn.com/video/1666894951-d3fc47ae1d5f09250c468b79e4063afa7142163b5734eacb2277ea28acd44b52-d?mw=1000&mh=1000' }
   ]
 
-	function createPreview(video) {
-		const videoElement = document.createElement('img')
-		videoElement.src = video.thumbnail
-		videoElement.classList.add('video-thumbnail')
-		videoElement.addEventListener('click', () => openModal(video))
-		return videoElement
-  }
+function createPreview(video) {
+	const videoElement = document.createElement('img')
+	videoElement.src = video.thumbnail
+	videoElement.classList.add('video-thumbnail')
+	videoElement.addEventListener('click', () => openModal(video))
+	return videoElement
+}
+
+function createPagi(video) {
+	const span = document.createElement('span')
+	span.addEventListener('click', () => openModal(video))
+	span.innerHTML = video.id + 1
+	return span
+}
 
 function openModal(videoId) {
+	if (document.querySelector('#player')) {
+		document.querySelector('#player').remove()
+	}
+
+	if (videoId.id === 7) {
+		rightArrow.style.display = 'none'
+	} else rightArrow.style.display = 'block'
+
+	if (videoId.id === 0) {
+		leftArrow.style.display = 'none'
+	} else leftArrow.style.display = 'block'
+
+	if (document.querySelector('.active')) {
+		document.querySelector('.active').classList.remove('active')
+	}
+
+	document.querySelectorAll('span')[videoId.id].classList.add('active')
+
+	active = videoId.id
+
 	modal.style.display = 'block'
-		const player = document.createElement('div')
-		player.id = 'player'
-		modalContent.prepend(player)
+
+	const player = document.createElement('div')
+	player.id = 'player'
+	modalContent.prepend(player)
+
 	new Vimeo.Player('player', {
 		id: videoId.url,
 		autoplay: true,
@@ -42,24 +75,41 @@ function openModal(videoId) {
 window.addEventListener('click', e => {
 	if (e.target.className === 'modal') {
 		modal.style.display = 'none'
-		document.querySelector('#player').remove()
 	}
 })
 
 window.addEventListener('keydown', e => {
 	if (e.code === "Escape") {
 		modal.style.display = 'none';
-		document.querySelector('#player').remove()
 	}
 })
 
-  videos.forEach((video, i) => {
+window.addEventListener('keydown', e => {
+	if(e.code === "ArrowRight" && active < 7) {
+		active += 1
+		openModal(videos[active])
+	}
+	if (e.code === "ArrowLeft" && active > 0) {
+		active -= 1
+		openModal(videos[active])
+	}
+})
+
+leftArrow.addEventListener('click', () => {
+	if (active > 0) {
+		active -= 1
+		openModal(videos[active])
+	}
+})
+
+rightArrow.addEventListener('click', () => {
+	if (active < 7) {
+		active += 1
+		openModal(videos[active])
+	}
+})
+
+videos.forEach((video) => {
     preview.append(createPreview(video))
-	const span = document.createElement('span')
-	span.addEventListener('click', () => {
-		document.querySelector('#player').remove()
-		openModal(video)
-	})
- 	span.innerHTML = i + 1
-  	pagi.append(span)
-  });
+  	pagi.append(createPagi(video))
+});
